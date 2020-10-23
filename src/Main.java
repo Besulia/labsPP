581272a44803b203a6fbc69476048a6084b4d224
Yulia2102 <yulia.bespalova21@gmail.com>
2020-10-23 03:05:38 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("JAVA HELLO");
+        String a="bla bla bla";
+        System.out.println(a);
     }
 }
