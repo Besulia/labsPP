46a594f3756fd18fddbe268252f70ebbb55b7134
Yulia2102 <yulia.bespalova21@gmail.com>
2020-10-30 22:47:29 +0200

    last commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,73 @@
+import java.util.Scanner;
+
 public class Main {
+    static int elem=2;
+    public static void masivA(Student[] arr) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Введіть назву факультету: ");
+        String faculty=scan.nextLine();
+        for(int i=0;i<elem;++i){
+            if(arr[i].getFaculty().equals(faculty))
+                System.out.println(arr[i].toString());
+        }
+    }
+
+    public static void masivB(Student[] arr) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Введіть рік народження: ");
+        String birthday=scan.nextLine();
+        String currBday="";
+        for(int i=0;i<elem;++i){
+            for(int j=6;j<10;j++ ){
+               currBday+= arr[i].getBirthday().toCharArray()[j];
+            }
+            if(Integer.parseInt(currBday)>Integer.parseInt(birthday))
+                System.out.println(arr[i].toString());
+            currBday="";
+        }
+    }
+    public static void masivC(Student[] arr) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Введіть назву групи: ");
+        String group=scan.nextLine();
+        for(int i=0;i<elem;++i){
+            if(arr[i].getGroup().equals(group))
+                System.out.println(arr[i].toString());
+        }
+    }
     public static void main(String[] args) {
-        String a="bla bla bla";
-        System.out.println(a);
+        Scanner scan=new Scanner(System.in);
+        Student[] arr = new Student[elem];
+         for(int i=0;i<elem;i++){
+             int id=0, course=0;
+             String surname, name, fathersname, birthday, address, number, faculty, group;
+
+            System.out.println("Введіть Id: ");
+            id= scan.nextInt();
+            scan.nextLine();
+            System.out.println("Введіть ім'я: ");
+            name=scan.nextLine();
+            System.out.println("Введіть прізвище: ");
+            surname=scan.nextLine();
+            System.out.println("Введіть по-батькові: ");
+            fathersname=scan.nextLine();
+            System.out.println("Введіть дату народження (dd.mm.rrrr): ");
+            birthday=scan.nextLine();
+            System.out.println("Введіть адресу: ");
+            address=scan.nextLine();
+            System.out.println("Введіть номер телефону: ");
+            number=scan.nextLine();
+            System.out.println("Введіть назву факультету: ");
+            faculty=scan.nextLine();
+            System.out.println("Введіть групу: ");
+            group=scan.nextLine();
+            System.out.println("Введіть курс: ");
+            course= scan.nextInt();
+            arr[i]=new Student(id,surname,name, fathersname, birthday,address,number,faculty,course,group);
+            System.out.println(arr[i].toString());
+        }
+        masivA(arr);
+        masivB(arr);
+        masivC(arr);
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,121 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private int id;
+    private String surname;
+    private String name;
+    private String fathersname;
+    private String birthday;
+    private String address;
+    private String number;
+    private String faculty;
+    private int course;
+    private String group;
+    Student(){
+        id=0; surname="";name=""; fathersname="";
+        birthday=""; address=""; number="";
+        faculty=""; course=0; group="";
+    }
+    Student(int id, String surname, String name,
+            String fathersname, String birthday, String address,
+            String number, String faculty,int course, String group )
+    {
+        this.id=id; this.surname=surname;this.name=name; this.fathersname=fathersname;
+        this.birthday=birthday; this.address=address; this.number=number;
+        this.faculty=faculty; this.course=course; this.group=group;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFathersname() {
+        return fathersname;
+    }
+
+    public void setFathersname(String fathersname) {
+        this.fathersname = fathersname;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", fathersname='" + fathersname + '\'' +
+                ", birthday='" + birthday + '\'' +
+                ", address='" + address + '\'' +
+                ", number='" + number + '\'' +
+                ", faculty='" + faculty + '\'' +
+                ", course=" + course +
+                ", group='" + group + '\'' +
+                '}';
+    }
 }
